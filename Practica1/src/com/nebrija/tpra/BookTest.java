12dd6ed5808d83c4831020e6f010eb7402699ba9
Álvaro Rodríguez Bonmatí <43890020+varorb99@users.noreply.github.com>
2018-11-12 12:27:26 +0100

    Upload files

---
+++ Practica1/src/com/nebrija/tpra/BookTest.java
@@ -0,0 +1,14 @@
+package com.nebrija.tpra;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	@Test
+	void testBook() {
+		fail("Not yet implemented");
+	}
+
+}
